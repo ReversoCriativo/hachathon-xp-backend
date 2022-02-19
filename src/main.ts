8dd072f585822c9b95dafffda913ec8ace9c011a
erickgato <itsbk0703@gmail.com>
2022-02-19 00:12:18 -0300

    feat: add health module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,39 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { author, name, version, description } from '../package.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+  app.enableCors({
+    origin: '*', // todo: add frontend url for production
+    credentials: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidUnknownValues: true,
+    }),
+  );
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle(`[${author}]-${name}`.toUpperCase())
+    .setDescription(description)
+    .setVersion(version)
+    .addTag('Documentation')
+    .addBearerAuth()
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('api/docs', app, swaggerDocument);
+
   await app.listen(3001);
 }
 bootstrap();
